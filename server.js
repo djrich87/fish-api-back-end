92e0282e38b2b47891d747a0a091bf495d1feeda
djrich87 <djrich87@gmail.com>
2022-04-03 23:51:45 -0600

    connect to front end

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
 import './config/database.js'
+import cors from 'cors'
 // import routers
 
 import { router as fishesRouter } from './routes/fishes.js'
@@ -20,6 +21,7 @@ app.set(
 app.set('view engine', 'ejs')
 
 // middleware
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -30,7 +32,6 @@ app.use(
 )
 
 // mounted routers
-
 app.use('/api/fishes', fishesRouter)
 
 // catch 404 and forward to error handler
